61f0059a48c3d3c4ded5853bf3219041aa3597e1
Jerry <dfarrugia6@gmail.com>
2023-02-19 15:34:49 +1100

    Push first pass

--- config/provider.go
+++ config/provider.go
@@ -10,12 +10,12 @@ import (
 
 	ujconfig "github.com/upbound/upjet/pkg/config"
 
-	"github.com/upbound/upjet-provider-template/config/null"
+	"github.com/dfarrugia/provider-databricks/config/null"
 )
 
 const (
-	resourcePrefix = "template"
-	modulePath     = "github.com/upbound/upjet-provider-template"
+	resourcePrefix = "databricks"
+	modulePath     = "github.com/dfarrugia/provider-databricks"
 )
 
 //go:embed schema.json
